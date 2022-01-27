9500e08a8d17acc1954172a26cea5fc81f51c0d7
KxxMxxx <lewkaiming123@gmail.com>
2022-01-27 22:27:57 +0800

    Add query clause type and refactor query clause

--- Team09/Code09/src/spa/src/models/Query.h
+++ Team09/Code09/src/spa/src/models/Query.h
@@ -1,21 +1,20 @@
 #pragma once
 
-#include "QueryArgumentType.h"
 #include "QueryArgument.h"
-#include "QueryClause.h"
 #include "QueryPatternClause.h"
+#include "QuerySuchThatClause.h"
 
 class Query {
 public:
-    Query(std::vector<QueryArgument>& resultSynonyms, std::vector<QueryClause>& suchThatClauses,
+    Query(std::vector<QueryArgument>& resultSynonyms, std::vector<QuerySuchThatClause>& suchThatClauses,
           std::vector<QueryPatternClause>& patternClauses);
 
     const std::vector<QueryArgument>& GetResultSynonyms() const;
-    const std::vector<QueryClause>& GetSuchThatClauses() const;
+    const std::vector<QuerySuchThatClause>& GetSuchThatClauses() const;
     const std::vector<QueryPatternClause>& GetPatternClauses() const;
 
 private:
     std::vector<QueryArgument> resultSynonyms;
-    std::vector<QueryClause> suchThatClauses;
+    std::vector<QuerySuchThatClause> suchThatClauses;
     std::vector<QueryPatternClause> patternClauses;
 };

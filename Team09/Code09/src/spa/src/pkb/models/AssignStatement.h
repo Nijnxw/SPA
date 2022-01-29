5590503971bcf253a8208b995ac365463bae60a7
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2022-01-30 02:51:42 +0800

    Add AssignStatement class

---
+++ Team09/Code09/src/spa/src/pkb/models/AssignStatement.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class AssignStatement {
+private:
+	int statementNumber;
+	std::string leftHandSide;
+	std::string rightHandSide;
+
+public:
+	AssignStatement(int statementNumber, std::string leftHandSide, std::string rightHandSide); 
+
+	int getStatementNumber();
+	std::string getLeftHandSide();
+	std::string getRightHandSide();
+
+	/* TODO: Consider adding equals method for comparison between AssignStatement objects, 
+	possibly using the RPN util */
+};

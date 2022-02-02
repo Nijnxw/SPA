77b70c5fae5352568620071d441f52bcf3f316b9
ryanlimjr <ryanlimjr@yahoo.com.sg>
2022-02-02 12:26:10 +0800

    add overloaded == operator to compare PQLTokens

--- Team09/Code09/src/spa/src/PQL/PQLToken.h
+++ Team09/Code09/src/spa/src/PQL/PQLToken.h
@@ -11,6 +11,10 @@ public:
 	const std::string& getValue() const;
 	TokenType getType();
 
+	bool PQLToken::operator==(const PQLToken& other) const {
+		return type == other.type && value == other.value;
+	}
+
 private:
 	std::string value;
 	TokenType type;

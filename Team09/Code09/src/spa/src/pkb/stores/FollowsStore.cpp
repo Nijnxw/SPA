f4fad2e8ac9037f8a811d3771a9991800a555615
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2022-02-02 23:44:35 +0800

    Add helper functions for FollowsStore

--- Team09/Code09/src/spa/src/pkb/stores/FollowsStore.cpp
+++ Team09/Code09/src/spa/src/pkb/stores/FollowsStore.cpp
@@ -15,9 +15,68 @@ void FollowsStore::clear() {
 }
 
 bool FollowsStore::addFollows(int follower, int followee) {
-	return true; // TODO: Implement functionality.
+	return followerToFollowee.insert({ follower, followee }).second && followeeToFollower.insert({ followee, follower }).second;
 }
 
 bool FollowsStore::addFollowsT(int follower, int followee) {
-	return true; // TODO: Implement functionality.
+	return PKBUtil::addToMapWithSet(followerToFolloweesT, follower, followee) && PKBUtil::addToMapWithSet(followeeToFollowersT, followee, follower);
+}
+
+bool FollowsStore::isFollows(int follower, int followee) {
+	if (followerToFollowee.count(follower) <= 0) {
+		return false;
+	}
+	return followerToFollowee.at(follower) == followee;
+}
+
+bool FollowsStore::isFollowsStar(int follower, int followee) {
+	if (followerToFolloweesT.count(follower) <= 0) {
+		return false;
+	}
+	std::unordered_set<int> followees = followerToFolloweesT[follower];
+	return followees.count(followee) > 0;
+}
+
+int FollowsStore::getFollowee(int follower) {
+	return followerToFollowee[follower];
+}
+
+int FollowsStore::getFollower(int followee) {
+	return followeeToFollower[followee];
+}
+
+std::unordered_set<int> FollowsStore::getAllFollowees(int follower) {
+	return PKBUtil::getKeySetFromMap(followeeToFollower);
+}
+
+std::unordered_set<int> FollowsStore::getAllFollowers(int followee) {
+	return PKBUtil::getKeySetFromMap(followerToFollowee);
+}
+
+std::unordered_set<int> FollowsStore::getFolloweesT(int follower) {
+	return followerToFolloweesT[follower];
+}
+
+std::unordered_set<int> FollowsStore::getFollowersT(int followee) {
+	return followeeToFollowersT[followee];
+}
+
+std::unordered_set<int> FollowsStore::getAllFolloweesT(int follower) {
+	return PKBUtil::getKeySetFromMap(followeeToFollowersT);
+}
+
+std::unordered_set<int> FollowsStore::getAllFollowersT(int followee) {
+	return PKBUtil::getKeySetFromMap(followerToFolloweesT);
+}
+
+QueryResultTable FollowsStore::getFollows(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
+{
+	if (LHS == RHS && LHSType != EntityType::WILD) {
+		// Empty table, Follows(var, var) is invalid. 
+	}
+}
+
+QueryResultTable FollowsStore::getFollowsT(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
+{
+
 }
--- Team09/Code09/src/spa/src/pkb/stores/FollowsStore.h
+++ Team09/Code09/src/spa/src/pkb/stores/FollowsStore.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include "pkb/util/PKBUtil.cpp"
+#include <models/QueryResultTable.h>
+#include <models/EntityType.h>
 
 class FollowsStore {
 private:
@@ -14,11 +17,29 @@ private:
 	static std::unordered_map<int, std::unordered_set<int>> followerToFolloweesT;
 	static std::unordered_map<int, std::unordered_set<int>> followeeToFollowersT;
 
+	static bool isFollows(int follower, int followee);
+	static bool isFollowsStar(int follower, int followee);
+
+	static int getFollowee(int follower);
+	static int getFollower(int followee);
+	static std::unordered_set<int> getAllFollowees(int follower);
+	static std::unordered_set<int> getAllFollowers(int followee);
+
+	static std::unordered_set<int> getFolloweesT(int follower);
+	static std::unordered_set<int> getFollowersT(int followee);
+	static std::unordered_set<int> getAllFolloweesT(int follower);
+	static std::unordered_set<int> getAllFollowersT(int followee);
+
 public:
 	FollowsStore();
 
 	static void clear();
 
+	// Called by SP and DE
 	static bool addFollows(int follower, int followee);
 	static bool addFollowsT(int follower, int followee);
+
+	// Called by QE
+	static QueryResultTable getFollows(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult);
+	static QueryResultTable getFollowsT(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult);
 };

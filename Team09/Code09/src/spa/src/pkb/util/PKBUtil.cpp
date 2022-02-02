accaca0e672f2a3e6e00d2a00bf9f1e4adced2a2
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2022-02-02 23:44:14 +0800

    Add PKB util functions for maps

--- Team09/Code09/src/spa/src/pkb/util/PKBUtil.cpp
+++ Team09/Code09/src/spa/src/pkb/util/PKBUtil.cpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <unordered_set>
+#include <unordered_map>
+#include <vector>
 
 namespace PKBUtil
 {
@@ -21,5 +23,46 @@ namespace PKBUtil
             }
         }
         return resultSet;
+    }
+
+    bool addToMapWithSet(std::unordered_map<int, std::unordered_set<int>>& map, const int key, const int value) {
+        bool isAdded = true;
+
+        if (map.count(key) <= 0) {
+            std::unordered_set<int> newSet;
+            isAdded = isAdded && newSet.insert(value).second;
+            isAdded = isAdded && map.insert({ key, newSet }).second;
+        }
+        else {
+            std::unordered_set<int> set = map.at(key);
+            isAdded = isAdded && map[key].insert(value).second;
+        }
+
+        return isAdded;
+    }
+
+    bool addToMapWithVector(std::unordered_map<int, std::vector<int>>& map, const int key, const int value) {
+        bool isAdded = true;
+
+        if (map.count(key) <= 0) {
+            std::vector<int> newVector;
+            newVector.push_back(value);
+            isAdded = isAdded && map.emplace(key, newVector).second;
+        }
+        else {
+            std::vector<int> set = map.at(key);
+            map.at(key).push_back(value);
+        }
+
+        return isAdded;
+    }
+
+    template <typename T, typename U, typename Hash, typename Pred, typename Alloc>
+    std::unordered_set<T> getKeySetFromMap(std::unordered_map<T, U, Hash, Pred, Alloc>& map) {
+        std::unordered_set<T> keySet;
+        for (auto const& pair : map) {
+            keySet.insert(pair.first);
+        }
+        return keySet;
     }
 }

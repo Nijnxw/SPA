6f8e6fbbdf52b71df0375f8f1d5b64de8f641227
fabbbbbbyy <fabianchia97@gmail.com>
2022-02-03 13:30:41 +0800

    Implement QE side API for FollowsStore

--- Team09/Code09/src/spa/src/pkb/stores/FollowsStore.cpp
+++ Team09/Code09/src/spa/src/pkb/stores/FollowsStore.cpp
@@ -22,6 +22,194 @@ bool FollowsStore::addFollowsT(int follower, int followee) {
 	return PKBUtil::addToMapWithSet(followerToFolloweesT, follower, followee) && PKBUtil::addToMapWithSet(followeeToFollowersT, followee, follower);
 }
 
+QueryResultTable FollowsStore::getFollows(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
+{
+	QueryResultTable queryResult;
+
+	switch (LHSType) {
+	case EntityType::INT:
+		queryResult = getFollowsByStatementNumber(LHS, RHS, RHSType);
+		break;
+	case EntityType::STMT:
+		queryResult = getFollowsByStatementVariable(LHS, RHS, RHSType);
+		break;
+	case EntityType::WILD:
+		queryResult = getFollowsByUnderscore(RHS, RHSType);
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable FollowsStore::getFollowsByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Follows(1, 2) 
+		if (isFollows(LHS, RHS)) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Follows(1, s)
+		if (isFollower(std::stoi(LHS))) {
+			queryResult.addColumn(RHS, getFollowee(LHS));
+		}
+		break;
+	case EntityType::WILD: // Follows(1, _)
+		if (isFollower(std::stoi(LHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable FollowsStore::getFollowsByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Follows(s, 2)
+		if (isFollowee(std::stoi(RHS))) {
+			queryResult.addColumn(LHS, getFollower(RHS));
+		}
+		break;
+	case EntityType::STMT: // Follows(s1, s2)
+		auto [followers, followees] = getAllFollows();
+		queryResult.addColumn(LHS, followers);
+		queryResult.addColumn(RHS, followees);
+		break;
+	case EntityType::WILD: // Follows(s, _)
+		queryResult.addColumn(LHS, getAllFollowers());
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+QueryResultTable FollowsStore::getFollowsByUnderscore(std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Follows(_, 2)
+		if (isFollowee(std::stoi(RHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Follows(_, s)
+		queryResult.addColumn(LHS, getAllFollowees());
+		break;
+	case EntityType::WILD: // Follows(_, _)
+		if (hasFollows()) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable FollowsStore::getFollowsT(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
+{
+	QueryResultTable queryResult;
+
+	switch (LHSType) {
+	case EntityType::INT:
+		queryResult = getFollowsTByStatementNumber(LHS, RHS, RHSType);
+		break;
+	case EntityType::STMT:
+		queryResult = getFollowsTByStatementVariable(LHS, RHS, RHSType);
+		break;
+	case EntityType::WILD:
+		queryResult = getFollowsTByUnderscore(RHS, RHSType);
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable FollowsStore::getFollowsTByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Follows*(1, 2) 
+		if (isFollowsT(LHS, RHS)) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Follows*(1, s)
+		if (isFollowerT(std::stoi(LHS))) {
+			queryResult.addColumn(RHS, getFolloweesT(LHS));
+		}
+		break;
+	case EntityType::WILD: // Follows*(1, _)
+		if (isFollowerT(std::stoi(LHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable FollowsStore::getFollowsTByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Follows*(s, 2)
+		if (isFollowee(std::stoi(RHS))) {
+			queryResult.addColumn(LHS, getFollowersT(RHS));
+		}
+		break;
+	case EntityType::STMT: // Follows*(s1, s2)
+		auto [followers, followees] = getAllFollowsT();
+		queryResult.addColumn(LHS, followers);
+		queryResult.addColumn(RHS, followees);
+		break;
+	case EntityType::WILD: // Follows*(s, _)
+		queryResult.addColumn(LHS, getAllFollowersT());
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+QueryResultTable FollowsStore::getFollowsTByUnderscore(std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Follows*(_, 2)
+		if (isFolloweeT(std::stoi(RHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Follows*(_, s)
+		queryResult.addColumn(LHS, getAllFolloweesT());
+		break;
+	case EntityType::WILD: // Follows*(_, _)
+		if (hasFollowsT()) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
 bool FollowsStore::isFollows(int follower, int followee) {
 	if (followerToFollowee.count(follower) <= 0) {
 		return false;
@@ -29,12 +217,40 @@ bool FollowsStore::isFollows(int follower, int followee) {
 	return followerToFollowee.at(follower) == followee;
 }
 
-bool FollowsStore::isFollowsStar(int follower, int followee) {
+bool FollowsStore::isFollowsT(int follower, int followee) {
 	if (followerToFolloweesT.count(follower) <= 0) {
 		return false;
 	}
 	std::unordered_set<int> followees = followerToFolloweesT[follower];
 	return followees.count(followee) > 0;
+}
+
+bool FollowsStore::hasFollows() {
+	return followerToFollowee.size() > 0;
+}
+
+bool FollowsStore::hasFollowsT() {
+	return followerToFolloweesT.size() > 0;
+}
+
+bool FollowsStore::isFollowee(int followee) {
+	return followeeToFollower.count(followee) > 0;
+}
+
+bool FollowsStore::isFollower(int follower) {
+	return followerToFollowee.count(follower) > 0;
+}
+
+bool FollowsStore::isFollowee(int followee) {
+	return followeeToFollower.count(followee) > 0;
+}
+
+bool FollowsStore::isFollowerT(int follower) {
+	return followerToFolloweesT.count(follower) > 0;
+}
+
+bool FollowsStore::isFolloweeT(int followee) {
+	return followeeToFollowersT.count(followee) > 0;
 }
 
 int FollowsStore::getFollowee(int follower) {
@@ -45,11 +261,11 @@ int FollowsStore::getFollower(int followee) {
 	return followeeToFollower[followee];
 }
 
-std::unordered_set<int> FollowsStore::getAllFollowees(int follower) {
+std::unordered_set<int> FollowsStore::getAllFollowees() {
 	return PKBUtil::getKeySetFromMap(followeeToFollower);
 }
 
-std::unordered_set<int> FollowsStore::getAllFollowers(int followee) {
+std::unordered_set<int> FollowsStore::getAllFollowers() {
 	return PKBUtil::getKeySetFromMap(followerToFollowee);
 }
 
@@ -61,22 +277,18 @@ std::unordered_set<int> FollowsStore::getFollowersT(int followee) {
 	return followeeToFollowersT[followee];
 }
 
-std::unordered_set<int> FollowsStore::getAllFolloweesT(int follower) {
+std::unordered_set<int> FollowsStore::getAllFolloweesT() {
 	return PKBUtil::getKeySetFromMap(followeeToFollowersT);
 }
 
-std::unordered_set<int> FollowsStore::getAllFollowersT(int followee) {
+std::unordered_set<int> FollowsStore::getAllFollowersT() {
 	return PKBUtil::getKeySetFromMap(followerToFolloweesT);
 }
 
-QueryResultTable FollowsStore::getFollows(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
-{
-	if (LHS == RHS && LHSType != EntityType::WILD) {
-		// Empty table, Follows(var, var) is invalid. 
-	}
+std::tuple<std::vector<int>, std::vector<int>> FollowsStore::getAllFollows() {
+	return PKBUtil::convertMapToVectorTuple(followerToFollowee);
 }
 
-QueryResultTable FollowsStore::getFollowsT(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
-{
-
+std::tuple<std::vector<int>, std::vector<int>> FollowsStore::getAllFollowsT() {
+	return PKBUtil::convertMapToVectorTuple(followerToFolloweesT);
 }
--- Team09/Code09/src/spa/src/pkb/stores/FollowsStore.h
+++ Team09/Code09/src/spa/src/pkb/stores/FollowsStore.h
@@ -17,18 +17,36 @@ private:
 	static std::unordered_map<int, std::unordered_set<int>> followerToFolloweesT;
 	static std::unordered_map<int, std::unordered_set<int>> followeeToFollowersT;
 
+	static bool hasFollows();
+	static bool hasFollowsT();
+
 	static bool isFollows(int follower, int followee);
-	static bool isFollowsStar(int follower, int followee);
+	static bool isFollowsT(int follower, int followee);
+	static bool isFollower(int follower);
+	static bool isFollowee(int followee);
+	static bool isFollowerT(int follower);
+	static bool isFolloweeT(int followee);
 
 	static int getFollowee(int follower);
 	static int getFollower(int followee);
-	static std::unordered_set<int> getAllFollowees(int follower);
-	static std::unordered_set<int> getAllFollowers(int followee);
+	static std::unordered_set<int> getAllFollowees();
+	static std::unordered_set<int> getAllFollowers();
 
 	static std::unordered_set<int> getFolloweesT(int follower);
 	static std::unordered_set<int> getFollowersT(int followee);
-	static std::unordered_set<int> getAllFolloweesT(int follower);
-	static std::unordered_set<int> getAllFollowersT(int followee);
+	static std::unordered_set<int> getAllFolloweesT();
+	static std::unordered_set<int> getAllFollowersT();
+
+	static std::tuple<std::vector<int>, std::vector<int>> FollowsStore::getAllFollows();
+	static std::tuple<std::vector<int>, std::vector<int>> FollowsStore::getAllFollowsT();
+
+	static QueryResultTable getFollowsByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getFollowsByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getFollowsByUnderscore(std::string& RHS, EntityType RHSType);
+
+	static QueryResultTable getFollowsTByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getFollowsTByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getFollowsTByUnderscore(std::string& RHS, EntityType RHSType);
 
 public:
 	FollowsStore();

55f4c59247be7435c722d55322f6d3526bf64107
fabbbbbbyy <fabianchia97@gmail.com>
2022-02-03 15:26:42 +0800

    Implement QE side API for ParentStore

--- Team09/Code09/src/spa/src/pkb/stores/ParentStore.cpp
+++ Team09/Code09/src/spa/src/pkb/stores/ParentStore.cpp
@@ -1,9 +1,8 @@
 #include "ParentStore.h"
 
-std::unordered_set<int> parentStatements;
-std::unordered_set<int> childStatements;
 std::unordered_map<int, std::unordered_set<int>> parentToChildren;
 std::unordered_map<int, int> childToParent;
+std::unordered_map<int, int> parentTPairs;
 std::unordered_map<int, std::unordered_set<int>> parentToChildrenT;
 std::unordered_map<int, std::unordered_set<int>> childToParentsT;
 
@@ -19,9 +18,281 @@ void ParentStore::clear() {
 }
 
 bool ParentStore::addParent(int parent, int child) {
-	return true; // TODO: Implement functionality.
+	return PKBUtil::addToMapWithSet(parentToChildren, parent, child) && childToParent.insert({child, parent}).second;
 }
 
 bool ParentStore::addParentT(int parent, int child) {
-	return true; // TODO: Implement functionality.
+	return parentTPairs.insert({ parent, child }).second && PKBUtil::addToMapWithSet(parentToChildrenT, parent, child) && PKBUtil::addToMapWithSet(childToParentsT, child, parent);
+}
+
+QueryResultTable ParentStore::getParent(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
+{
+	QueryResultTable queryResult;
+
+	switch (LHSType) {
+	case EntityType::INT:
+		queryResult = getParentByStatementNumber(LHS, RHS, RHSType);
+		break;
+	case EntityType::STMT:
+		queryResult = getParentByStatementVariable(LHS, RHS, RHSType);
+		break;
+	case EntityType::WILD:
+		queryResult = getParentByUnderscore(RHS, RHSType);
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable ParentStore::getParentByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Parent(1, 2) 
+		if (isParentRelationship(LHS, RHS)) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Parent(1, s)
+		if (isParent(std::stoi(LHS))) {
+			queryResult.addColumn(RHS, getChildren(LHS));
+		}
+		break;
+	case EntityType::WILD: // Parent(1, _)
+		if (isParent(std::stoi(LHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable ParentStore::getParentByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Parent(s, 2)
+		if (isChild(std::stoi(RHS))) {
+			std::vector<int> parent;
+			parent.push_back(getParent(RHS));
+			queryResult.addColumn(LHS, parent);
+		}
+		break;
+	case EntityType::STMT: // Parent(s1, s2)
+		auto [parents, children] = getAllParentPairs();
+		queryResult.addColumn(LHS, parents);
+		queryResult.addColumn(RHS, children);
+		break;
+	case EntityType::WILD: // Parent(s, _)
+		queryResult.addColumn(LHS, getAllParents());
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+QueryResultTable ParentStore::getParentByUnderscore(std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Parent(_, 2)
+		if (isChild(std::stoi(RHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Parent(_, s)
+		queryResult.addColumn(LHS, getAllChildren());
+		break;
+	case EntityType::WILD: // Parent(_, _)
+		if (hasParentRelationship()) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable ParentStore::getParentT(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult)
+{
+	QueryResultTable queryResult;
+
+	switch (LHSType) {
+	case EntityType::INT:
+		queryResult = getParentTByStatementNumber(LHS, RHS, RHSType);
+		break;
+	case EntityType::STMT:
+		queryResult = getParentTByStatementVariable(LHS, RHS, RHSType);
+		break;
+	case EntityType::WILD:
+		queryResult = getParentTByUnderscore(RHS, RHSType);
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable ParentStore::getParentTByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Parent*(1, 2) 
+		if (isParentTRelationship(LHS, RHS)) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Parent*(1, s)
+		if (isParentT(std::stoi(LHS))) {
+			queryResult.addColumn(RHS, getChildrenT(LHS));
+		}
+		break;
+	case EntityType::WILD: // Parent*(1, _)
+		if (isParentT(std::stoi(LHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+QueryResultTable ParentStore::getParentByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Parent*(s, 2)
+		if (isChildT(std::stoi(RHS))) {
+			std::vector<int> parent;
+			parent.push_back(getParentT(RHS));
+			queryResult.addColumn(LHS, parent);
+		}
+		break;
+	case EntityType::STMT: // Parent*(s1, s2)
+		auto [parents, children] = getAllParentTPairs();
+		queryResult.addColumn(LHS, parents);
+		queryResult.addColumn(RHS, children);
+		break;
+	case EntityType::WILD: // Parent*(s, _)
+		queryResult.addColumn(LHS, getAllParentsT());
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+QueryResultTable ParentStore::getParentTByUnderscore(std::string& RHS, EntityType RHSType) {
+	QueryResultTable queryResult;
+
+	switch (RHSType) {
+	case EntityType::INT: // Parent*(_, 2)
+		if (isChildT(std::stoi(RHS))) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	case EntityType::STMT: // Parent*(_, s)
+		queryResult.addColumn(LHS, getAllChildrenT());
+		break;
+	case EntityType::WILD: // Parent*(_, _)
+		if (hasParentTRelationship()) {
+			queryResult.setBooleanResult(true);
+		}
+		break;
+	default:
+		break;
+	}
+
+	return queryResult;
+}
+
+bool ParentStore::hasParentRelationship() {
+	return parentToChildren.size() > 0;
+}
+
+bool ParentStore::hasParentTRelationship() {
+	return parentToChildrenT.size() > 0;
+}
+
+bool ParentStore::isParentRelationship(int parent, int child) {
+	if (parentToChildren.count(parent) <= 0) {
+		return false;
+	}
+	std::unordered_set<int> children = parentToChildren[parent];
+	return children.count(child) > 0;
+}
+
+bool ParentStore::isParentTRelationship(int parent, int child) {
+	if (parentToChildrenT.count(parent) <= 0) {
+		return false;
+	}
+	std::unordered_set<int> children = parentToChildrenT[parent];
+	return children.count(child) > 0;
+}
+
+bool ParentStore::isParent(int parent) {
+	return parentToChildren.count(parent) > 0;
+}
+
+bool ParentStore::isChild(int child) {
+	return childToParent.count(child) > 0;
+}
+
+bool ParentStore::isParentT(int parent) {
+	return parentToChildrenT.count(parent) > 0;
+}
+
+bool ParentStore::isChildT(int child) {
+	return childToParentsT.count(child) > 0;
+}
+
+std::unordered_set<int> ParentStore::getChildren(int parent) {
+	return parentToChildren[parent];
+}
+
+int ParentStore::getParent(int child) {
+	return childToParent[child];
+}
+
+std::unordered_set<int> ParentStore::getAllChildren() {
+	return PKBUtil::getKeySetFromMap(childToParent);
+}
+
+std::unordered_set<int> ParentStore::getAllParents() {
+	return PKBUtil::getKeySetFromMap(parentToChildren);
+}
+
+std::unordered_set<int> ParentStore::getChildrenT(int parent) {
+	return parentToChildrenT[parent];
+}
+
+std::unordered_set<int> ParentStore::getParentT(int child) {
+	return childToParentsT[child];
+}
+
+std::unordered_set<int> ParentStore::getAllChildrenT() {
+	return PKBUtil::getKeySetFromMap(childToParentsT);
+}
+
+std::unordered_set<int> ParentStore::getAllParentsT() {
+	return PKBUtil::getKeySetFromMap(parentToChildrenT);
+}
+
+std::tuple<std::vector<int>, std::vector<int>> ParentStore::getAllParentPairs() {
+	return PKBUtil::convertMapToVectorTuple(parentToChildren);
+}
+
+std::tuple<std::vector<int>, std::vector<int>> ParentStore::getAllParentTPairs() {
+	return PKBUtil::convertMapToVectorTuple(parentTPairs);
 }
--- Team09/Code09/src/spa/src/pkb/stores/ParentStore.h
+++ Team09/Code09/src/spa/src/pkb/stores/ParentStore.h
@@ -6,21 +6,59 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include "pkb/util/PKBUtil.cpp"
+#include <models/QueryResultTable.h>
+#include <models/EntityType.h>
 
 class ParentStore {
 private:
-	static std::unordered_set<int> parentStatements;
-	static std::unordered_set<int> childStatements;
 	static std::unordered_map<int, std::unordered_set<int>> parentToChildren;
 	static std::unordered_map<int, int> childToParent;
+	static std::unordered_map<int, int> parentTPairs;
 	static std::unordered_map<int, std::unordered_set<int>> parentToChildrenT;
 	static std::unordered_map<int, std::unordered_set<int>> childToParentsT;
+
+	static bool hasParentRelationship();
+	static bool hasParentTRelationship();
+
+	static bool isParentRelationship(int parent, int child);
+	static bool isParentTRelationship(int parent, int child);
+	static bool isParent(int parent);
+	static bool isChild(int child);
+	static bool isParentT(int parent);
+	static bool isChildT(int child);
+
+	static std::unordered_set<int> getChildren(int parent);
+	static int getParent(int child);
+	static std::unordered_set<int> getAllChildren();
+	static std::unordered_set<int> getAllParents();
+
+	static std::unordered_set<int> getChildrenT(int parent);
+	static std::unordered_set<int> getParentsT(int child);
+	static std::unordered_set<int> getAllChildrenT();
+	static std::unordered_set<int> getAllParentsT();
+
+	static std::tuple<std::vector<int>, std::vector<int>> getAllParentPairs();
+	static std::tuple<std::vector<int>, std::vector<int>> getAllParentTPairs();
+
+	static QueryResultTable getParentByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getParentByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getParentByUnderscore(std::string& RHS, EntityType RHSType);
+
+	static QueryResultTable getParentTByStatementNumber(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getParentTByStatementVariable(std::string& LHS, std::string& RHS, EntityType RHSType);
+	static QueryResultTable getParentTByUnderscore(std::string& RHS, EntityType RHSType);
 
 public:
 	ParentStore();
 
 	static void clear();
 
+	// Called by SP and DE
 	static bool addParent(int parent, int child);
 	static bool addParentT(int parent, int child);
+
+	// Called by QE
+	static QueryResultTable getParent(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult);
+	static QueryResultTable getParentT(std::string& LHS, std::string& RHS, EntityType LHSType, EntityType RHSType, bool isBooleanResult);
 };

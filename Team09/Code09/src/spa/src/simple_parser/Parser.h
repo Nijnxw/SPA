57b66113fb195cb3594066828017a859a6232c1c
Wen Jin <wenjin0257@gmail.com>
2022-02-04 11:45:58 +0800

    Implement SIMPLE parser for read stmts

--- Team09/Code09/src/spa/src/simple_parser/Parser.h
+++ Team09/Code09/src/spa/src/simple_parser/Parser.h
@@ -1,3 +1,35 @@
 #pragma once
 
-int Parse();
+#include "simple_parser/Token.h"
+#include "models/simple_parser/RootNode.h"
+#include "models/simple_parser/ExprNodes.h"
+#include "models/simple_parser/IoNodes.h"
+
+#include <vector>
+
+class Parser {
+private:
+	int currentIdx = 0;
+	int stmtNo = 1;
+	std::vector<Token*> tokens;
+
+	Token* peek();
+	Token* get();
+
+	bool isEndOfFile();
+	bool check(const std::string& s);
+	bool check(TokenType t);
+	bool expect(const std::string& s);
+
+	std::shared_ptr<VariableNode> parseVariable();
+	std::shared_ptr<ProcedureNode> parseProcedure();
+
+	std::vector<std::shared_ptr<StmtNode>> parseStmtLst();
+	std::shared_ptr<StmtNode> parseStatement();
+	std::shared_ptr<ReadNode> parseRead();
+
+public:
+	explicit Parser(std::vector<Token*> tokens);
+
+	std::shared_ptr<RootNode> parseProgram();
+};

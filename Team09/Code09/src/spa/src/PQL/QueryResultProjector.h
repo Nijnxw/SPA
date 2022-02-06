4695bde4e216434b745dbee8bf4cf7f0f1a466ee
KxxMxxx <lewkaiming123@gmail.com>
2022-02-06 23:39:39 +0800

    Refactor query utils stringifyrows

--- Team09/Code09/src/spa/src/PQL/QueryResultProjector.h
+++ Team09/Code09/src/spa/src/PQL/QueryResultProjector.h
@@ -2,8 +2,9 @@
 
 #include "models/QueryClauseTable.h"
 #include "models/QueryArgument.h"
+#include <list>
 
 class QueryResultProjector {
 public:
-	static std::unordered_set<std::string> formatResult(Table& res, std::vector<QueryArgument>& selectSyn);
+	static std::list<std::string> formatResult(Table& res, std::vector<QueryArgument>& selectSyn);
 };

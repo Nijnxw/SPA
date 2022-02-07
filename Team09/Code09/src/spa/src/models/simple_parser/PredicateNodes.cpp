e38b7500da8b2b67eeb6004931e5468898f5cd6c
SoulUseless <jkyap99@gmail.com>
2022-02-08 00:54:49 +0800

    Implement RelExprNode for a comparison clause in a predicate

--- Team09/Code09/src/spa/src/models/simple_parser/PredicateNodes.cpp
+++ Team09/Code09/src/spa/src/models/simple_parser/PredicateNodes.cpp
@@ -1,1 +1,35 @@
 #include "PredicateNodes.h"
+
+RelExprNode::RelExprNode(RelFactorNode left, ComparatorOperator oper, RelFactorNode right) :
+	Node(NodeType::RELEXPR) {
+	lhs = left;
+	op = oper;
+	rhs = right;
+}
+
+RelFactorNode RelExprNode::getLhs() const {
+	return lhs;
+}
+
+RelFactorNode RelExprNode::getRhs() const {
+	return rhs;
+}
+
+ComparatorOperator RelExprNode::getOperator() const {
+	return op;
+}
+
+
+bool RelExprNode::operator==(const Node& other) const {
+	const RelExprNode* cast = dynamic_cast<const RelExprNode*> (&other);
+	return cast != nullptr &&
+		op == cast->getOperator() &&
+		std::visit(
+			[](const auto& thisLhs, const auto& otherLhs) { return *thisLhs == *otherLhs; },
+			lhs,
+			cast->getLhs()) && 
+		std::visit(
+			[](const auto& thisRhs, const auto& otherRhs) { return *thisRhs == *otherRhs; },
+			rhs,
+			cast->getRhs());
+}
--- Team09/Code09/src/spa/src/models/simple_parser/PredicateNodes.h
+++ Team09/Code09/src/spa/src/models/simple_parser/PredicateNodes.h
@@ -4,7 +4,7 @@
 #include "NodeType.h"
 #include "ExprNodes.h"
 
-#include <memory>;
+#include <memory>
 #include <variant>
 
 using RelFactorNode =
@@ -18,28 +18,29 @@ private:
 
 public:
 	RelExprNode(RelFactorNode left, ComparatorOperator oper, RelFactorNode right);
-	RelFactorNode getLhs();
-	RelFactorNode getRhs();
+	RelFactorNode getLhs() const;
+	RelFactorNode getRhs() const;
+	ComparatorOperator getOperator() const;
 	bool operator==(const Node& other) const override;
 };
 
-class PredicateNode : public Node {
-private:
-	std::shared_ptr<RelExprNode> relExpr;
-	std::shared_ptr<PredicateNode> lhs;
-	std::shared_ptr<ConditionalOperator> op;
-	std::shared_ptr<PredicateNode> rhs;
-
-public:
-	PredicateNode(RelExprNode relExpr);
-	PredicateNode(ConditionalOperator op, PredicateNode rhs);
-	PredicateNode(PredicateNode lhs, ConditionalOperator op, PredicateNode rhs);
-
-	std::shared_ptr<RelExprNode> getRelExprNode();
-	std::shared_ptr<PredicateNode> getLhs();
-	std::shared_ptr<PredicateNode> getRhs();
-
-	bool isTerminalPredicate();
-
-	bool operator==(const Node& other) const override;
-};
+//class PredicateNode : public Node {
+//private:
+//	std::shared_ptr<RelExprNode> relExpr;
+//	std::shared_ptr<PredicateNode> lhs;
+//	std::shared_ptr<ConditionalOperator> op;
+//	std::shared_ptr<PredicateNode> rhs;
+//
+//public:
+//	PredicateNode(RelExprNode relExpr);
+//	PredicateNode(ConditionalOperator op, PredicateNode rhs);
+//	PredicateNode(PredicateNode lhs, ConditionalOperator op, PredicateNode rhs);
+//
+//	std::shared_ptr<RelExprNode> getRelExprNode();
+//	std::shared_ptr<PredicateNode> getLhs();
+//	std::shared_ptr<PredicateNode> getRhs();
+//
+//	bool isTerminalPredicate();
+//
+//	bool operator==(const Node& other) const override;
+//};

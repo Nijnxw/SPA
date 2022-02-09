993c69817b5c6f11a3b8c2cf5a7c8b71678c6f22
SoulUseless <jkyap99@gmail.com>
2022-02-09 11:49:21 +0800

    Implement basic Follows unit tests

--- Team09/Code09/src/unit_testing/src/simple_parser/TestDesignExtractor.cpp
+++ Team09/Code09/src/unit_testing/src/simple_parser/TestDesignExtractor.cpp
@@ -11,6 +11,8 @@
 #include <unordered_set>
 #include <vector>
 
+//missing basic single line test cases
+
 TEST_CASE("Extract simple SIMPLE code no design entities") {
 	EntityStager::clear();
 
@@ -67,6 +69,85 @@ TEST_CASE("Extract simple SIMPLE code no design entities") {
 	std::unordered_set<int> expected_read_table;
 	expected_read_table.insert(1);
 	REQUIRE(EntityStager::getStagedReadStatements() == expected_read_table);
+
+	EntityStager::clear();
+}
+
+TEST_CASE("Basic Follows") {
+	EntityStager::clear();
+
+	/*
+	 * Test Simple Program
+	 * procedure testProgram {
+	 * 1	read x;
+	 * 2	print x;
+	 * }
+	 */
+
+	 // building AST
+	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
+
+	std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
+	std::shared_ptr<PrintNode> stmt2 = std::make_shared<PrintNode>(2, x);
+
+	std::vector<std::shared_ptr<StmtNode>> stmtList;
+	stmtList.push_back(stmt1);
+	stmtList.push_back(stmt2);
+
+	std::shared_ptr<ProcedureNode> proc1 = std::make_shared<ProcedureNode>(stmtList, "testProgram");
+	std::vector<std::shared_ptr<ProcedureNode>> procList;
+	procList.push_back(proc1);
+
+	AST ast = std::make_shared<ProgramNode>(procList);
+
+	DesignExtractor::extractDesignElements(ast);
+
+	std::vector<std::pair<int, int>> expectedFollows;
+	expectedFollows.push_back(std::make_pair(1, 2));
+
+	REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
+
+	EntityStager::clear();
+}
+
+TEST_CASE("Basic Follows - 3 lines") {
+	EntityStager::clear();
+
+	/*
+	 * Test Simple Program
+	 * procedure testProgram {
+	 * 1	read x;
+	 * 2	print x;
+	 * 3	print y;
+	 * }
+	 */
+
+	 // building AST
+	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
+	std::shared_ptr<VariableNode> y = std::make_shared<VariableNode>("y");
+
+	std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
+	std::shared_ptr<PrintNode> stmt2 = std::make_shared<PrintNode>(2, x);
+	std::shared_ptr<PrintNode> stmt3 = std::make_shared<PrintNode>(3, y);
+
+	std::vector<std::shared_ptr<StmtNode>> stmtList;
+	stmtList.push_back(stmt1);
+	stmtList.push_back(stmt2);
+	stmtList.push_back(stmt3);
+
+	std::shared_ptr<ProcedureNode> proc1 = std::make_shared<ProcedureNode>(stmtList, "testProgram");
+	std::vector<std::shared_ptr<ProcedureNode>> procList;
+	procList.push_back(proc1);
+
+	AST ast = std::make_shared<ProgramNode>(procList);
+
+	DesignExtractor::extractDesignElements(ast);
+
+	std::vector<std::pair<int, int>> expectedFollows;
+	expectedFollows.push_back(std::make_pair(1, 2));
+	expectedFollows.push_back(std::make_pair(2, 3));
+
+	REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
 
 	EntityStager::clear();
 }

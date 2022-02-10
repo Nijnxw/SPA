915a5ea7a9d930663776e4e5717b7817a95459e1
fabbbbbbyy <fabianchia97@gmail.com>
2022-02-10 11:29:04 +0800

    Refactor for modularity

--- Team09/Code09/src/spa/src/PQL/evaluators/StatementOnlyRelationshipEvaluator.h
+++ Team09/Code09/src/spa/src/PQL/evaluators/StatementOnlyRelationshipEvaluator.h
@@ -13,21 +13,13 @@
 
 class StatementOnlyRelationshipEvaluator : public RelationshipEvaluatorInterface, public TransitiveRelationshipEvaluatorInterface {
 private:
-	RelationRef relationship;
-	QueryClauseTable
+	static QueryClauseTable
 		getRelationshipByStatementNumber(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType RHSType);
-	QueryClauseTable
+	static QueryClauseTable
 		getRelationshipByStatementVariable(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType RHSType);
-	QueryClauseTable getRelationshipByUnderscore(RelationRef relationship, const std::string& RHS, EntityType RHSType);
+	static QueryClauseTable getRelationshipByUnderscore(RelationRef relationship, const std::string& RHS, EntityType RHSType);
 
-	QueryClauseTable
-		getRelationshipTByStatementNumber(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType RHSType);
-	QueryClauseTable
-		getRelationshipTByStatementVariable(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType RHSType);
-	QueryClauseTable getRelationshipTByUnderscore(RelationRef relationship, const std::string& RHS, EntityType RHSType);
 public:
 	static QueryClauseTable getRelationship(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
-		bool isBooleanResult);
-	static QueryClauseTable getTransitiveRelationship(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 		bool isBooleanResult);
 };

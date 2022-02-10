aec0c0b81e3ce53178350f127bc254eb37d02dc0
fabbbbbbyy <fabianchia97@gmail.com>
2022-02-10 11:28:50 +0800

    Fix linker errors

--- Team09/Code09/src/spa/src/PQL/evaluators/FollowsEvaluator.cpp
+++ Team09/Code09/src/spa/src/PQL/evaluators/FollowsEvaluator.cpp
@@ -1,13 +1,13 @@
 #include "FollowsEvaluator.h"
 
-static QueryClauseTable
-getFollows(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
+QueryClauseTable
+FollowsEvaluator::getFollows(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 	bool isBooleanResult) {
 	return StatementOnlyRelationshipEvaluator::getRelationship(RelationRef::FOLLOWS, LHS, RHS, LHSType, RHSType, isBooleanResult);
 }
 
-static QueryClauseTable
-getFollowsT(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
+QueryClauseTable
+FollowsEvaluator::getFollowsT(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 	bool isBooleanResult) {
-	return StatementOnlyRelationshipEvaluator::getTransitiveRelationship(RelationRef::FOLLOWS, LHS, RHS, LHSType, RHSType, isBooleanResult);
+	return StatementOnlyRelationshipEvaluator::getRelationship(RelationRef::FOLLOWS_T, LHS, RHS, LHSType, RHSType, isBooleanResult);
 }
--- Team09/Code09/src/spa/src/PQL/evaluators/ParentEvaluator.cpp
+++ Team09/Code09/src/spa/src/PQL/evaluators/ParentEvaluator.cpp
@@ -1,13 +1,13 @@
 #include "ParentEvaluator.h"
 
-static QueryClauseTable
-getParent(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
+QueryClauseTable
+ParentEvaluator::getParent(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 	bool isBooleanResult) {
 	return StatementOnlyRelationshipEvaluator::getRelationship(RelationRef::PARENT, LHS, RHS, LHSType, RHSType, isBooleanResult);
 }
 
-static QueryClauseTable
-getParentT(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
+QueryClauseTable
+ParentEvaluator::getParentT(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 	bool isBooleanResult) {
-	return StatementOnlyRelationshipEvaluator::getTransitiveRelationship(RelationRef::PARENT, LHS, RHS, LHSType, RHSType, isBooleanResult);
+	return StatementOnlyRelationshipEvaluator::getRelationship(RelationRef::PARENT_T, LHS, RHS, LHSType, RHSType, isBooleanResult);
 }

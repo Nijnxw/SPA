7d143c1b57172fb8c6ae186a869627ab668d873f
KxxMxxx <lewkaiming123@gmail.com>
2022-02-13 18:04:12 +0800

    Refactor DesignExtractor follows relationship unit tests

--- Team09/Code09/src/unit_testing/src/simple_parser/TestDesignExtractor_Follows.cpp
+++ Team09/Code09/src/unit_testing/src/simple_parser/TestDesignExtractor_Follows.cpp
@@ -1,5 +1,4 @@
 #include "catch.hpp"
-#include "models/EntityType.h"
 #include "models/simple_parser/AST.h"
 #include "models/simple_parser/ExprNodes.h"
 #include "models/simple_parser/IoNodes.h"
@@ -8,78 +7,78 @@
 #include "simple_parser/DesignExtractor.h"
 
 #include <memory>
-#include <unordered_set>
 #include <vector>
 
-TEST_CASE("Follows - Basic IO") {
-	EntityStager::clear();
-
-	/*
-	 * Test Simple Program
-	 * procedure testProgram {
-	 * 1	read x;
-	 * 2	print x;
-	 * }
-	 */
-
-	 // building AST
-	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
-
-	std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
-	std::shared_ptr<PrintNode> stmt2 = std::make_shared<PrintNode>(2, x);
-
-	std::vector<std::shared_ptr<StmtNode>> stmtList{ stmt1, stmt2 };
-
-	std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
-	std::vector<std::shared_ptr<ProcedureNode>> procList{ proc };
-
-	AST ast = std::make_shared<ProgramNode>(procList);
-
-	DesignExtractor::extractDesignElements(ast);
-
-	std::vector<std::pair<int, int>> expectedFollows{ std::make_pair(1, 2) };
-
-	REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
-
-	EntityStager::clear();
-}
-
-TEST_CASE("Follows - 3 lines - Basic IO") {
-	EntityStager::clear();
-
-	/*
-	 * Test Simple Program
-	 * procedure testProgram {
-	 * 1	read x;
-	 * 2	print x;
-	 * 3	print y;
-	 * }
-	 */
-
-	 // building AST
+TEST_CASE("Follows") {
 	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
 	std::shared_ptr<VariableNode> y = std::make_shared<VariableNode>("y");
 
-	std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
-	std::shared_ptr<PrintNode> stmt2 = std::make_shared<PrintNode>(2, x);
-	std::shared_ptr<PrintNode> stmt3 = std::make_shared<PrintNode>(3, y);
+	SECTION ("Basic IO") {
+		EntityStager::clear();
 
-	std::vector<std::shared_ptr<StmtNode>> stmtList{ stmt1, stmt2, stmt3 };
+		/*
+		 * Test Simple Program
+		 * procedure testProgram {
+		 * 1	read x;
+		 * 2	print x;
+		 * }
+		 */
 
-	std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
-	std::vector<std::shared_ptr<ProcedureNode>> procList{ proc };
+		std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
+		std::shared_ptr<PrintNode> stmt2 = std::make_shared<PrintNode>(2, x);
 
-	AST ast = std::make_shared<ProgramNode>(procList);
+		// set up procedure
+		std::vector<std::shared_ptr<StmtNode>> stmtList{stmt1, stmt2};
+		std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
+		// set up program
+		std::vector<std::shared_ptr<ProcedureNode>> procList{proc};
 
-	DesignExtractor::extractDesignElements(ast);
+		AST ast = std::make_shared<ProgramNode>(procList);
 
-	std::vector<std::pair<int, int>> expectedFollows{
-		std::make_pair(1, 2), std::make_pair(2, 3)
-	};
+		DesignExtractor::extractDesignElements(ast);
 
-	REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
+		std::vector<std::pair<int, int>> expectedFollows{{1, 2}};
 
-	EntityStager::clear();
+		REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
+
+		EntityStager::clear();
+	}
+
+	SECTION("3 lines - Basic IO") {
+		EntityStager::clear();
+
+		/*
+		 * Test Simple Program
+		 * procedure testProgram {
+		 * 1	read x;
+		 * 2	print x;
+		 * 3	print y;
+		 * }
+		 */
+
+		std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
+		std::shared_ptr<PrintNode> stmt2 = std::make_shared<PrintNode>(2, x);
+		std::shared_ptr<PrintNode> stmt3 = std::make_shared<PrintNode>(3, y);
+
+		// set up procedure
+		std::vector<std::shared_ptr<StmtNode>> stmtList{stmt1, stmt2, stmt3};
+		std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
+		// set up program
+		std::vector<std::shared_ptr<ProcedureNode>> procList{proc};
+
+		AST ast = std::make_shared<ProgramNode>(procList);
+
+		DesignExtractor::extractDesignElements(ast);
+
+		std::vector<std::pair<int, int>> expectedFollows{
+			{1, 2},
+			{2, 3}
+		};
+
+		REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
+
+		EntityStager::clear();
+	}
 }
 
 TEST_CASE("Follows - With Assignment") {
@@ -93,25 +92,26 @@ TEST_CASE("Follows - With Assignment") {
 	 * }
 	 */
 
-	 // building AST
 	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
-	std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
-
 	ExprNode y = std::make_shared<VariableNode>("y");
 	ExprNode one = std::make_shared<ConstantNode>("1");
+
+	std::shared_ptr<ReadNode> stmt1 = std::make_shared<ReadNode>(1, x);
+
 	ExprNode expression = std::make_shared<BinaryOperatorNode>(BinaryOperator::PLUS, y, one);
 	std::shared_ptr<AssignNode> stmt2 = std::make_shared<AssignNode>(2, x, expression, "y 1 +");
 
-	std::vector<std::shared_ptr<StmtNode>> stmtList{ stmt1, stmt2 };
-
+	// set up procedure
+	std::vector<std::shared_ptr<StmtNode>> stmtList{stmt1, stmt2};
 	std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
-	std::vector<std::shared_ptr<ProcedureNode>> procList{ proc };
+	// set up program
+	std::vector<std::shared_ptr<ProcedureNode>> procList{proc};
 
 	AST ast = std::make_shared<ProgramNode>(procList);
 
 	DesignExtractor::extractDesignElements(ast);
 
-	std::vector<std::pair<int, int>> expectedFollows{ std::make_pair(1, 2) };
+	std::vector<std::pair<int, int>> expectedFollows{{1, 2}};
 
 	REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
 
@@ -132,7 +132,7 @@ TEST_CASE("Follows - With While") {
 	 * }
 	 */
 
-	 // building AST
+	// building AST
 	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
 	std::shared_ptr<VariableNode> y = std::make_shared<VariableNode>("y");
 	std::shared_ptr<VariableNode> z = std::make_shared<VariableNode>("z");
@@ -141,27 +141,26 @@ TEST_CASE("Follows - With While") {
 	std::shared_ptr<RelExprNode> rel = std::make_shared<RelExprNode>(y, ComparatorOperator::GT, one);
 	std::shared_ptr<PredicateNode> pred = std::make_shared<PredicateNode>(rel);
 
-	std::vector<std::shared_ptr<StmtNode>> whileStmtList;
 	std::shared_ptr<ReadNode> read1 = std::make_shared<ReadNode>(2, x);
 	std::shared_ptr<ReadNode> read2 = std::make_shared<ReadNode>(3, z);
-	whileStmtList.push_back(read1);
-	whileStmtList.push_back(read2);
+	std::vector<std::shared_ptr<StmtNode>> whileStmtList{read1, read2};
 	std::shared_ptr<WhileNode> whiles = std::make_shared<WhileNode>(1, pred, whileStmtList);
 
 	std::shared_ptr<ReadNode> read3 = std::make_shared<ReadNode>(4, y);
-	std::vector<std::shared_ptr<StmtNode>> stmtList;
-	stmtList.push_back(whiles);
-	stmtList.push_back(read3);
 
+	// set up procedure
+	std::vector<std::shared_ptr<StmtNode>> stmtList{whiles, read3};
 	std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
-	std::vector<std::shared_ptr<ProcedureNode>> procList{ proc };
+	// set up program
+	std::vector<std::shared_ptr<ProcedureNode>> procList{proc};
 
 	AST ast = std::make_shared<ProgramNode>(procList);
 
 	DesignExtractor::extractDesignElements(ast);
 
 	std::vector<std::pair<int, int>> expectedFollows{
-		std::make_pair(1, 4), std::make_pair(2, 3)
+		{1, 4},
+		{2, 3}
 	};
 
 	REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
@@ -170,9 +169,17 @@ TEST_CASE("Follows - With While") {
 }
 
 TEST_CASE("Follows - With If") {
-	EntityStager::clear();
+	std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
+	std::shared_ptr<VariableNode> y = std::make_shared<VariableNode>("y");
+	std::shared_ptr<VariableNode> z = std::make_shared<VariableNode>("z");
+	RelFactorNode one = std::make_shared<ConstantNode>("1");
+
+	// cond expression
+	std::shared_ptr<RelExprNode> yGTOne = std::make_shared<RelExprNode>(y, ComparatorOperator::GT, one);
 
 	SECTION("Single if - simple predicate") {
+		EntityStager::clear();
+
 		/*
 		 * Test Simple Program
 		 * procedure testProgram {
@@ -185,41 +192,34 @@ TEST_CASE("Follows - With If") {
 		 *  }
 		 */
 
-		 // building AST
-		std::shared_ptr<VariableNode> x = std::make_shared<VariableNode>("x");
-		std::shared_ptr<VariableNode> y = std::make_shared<VariableNode>("y");
-		std::shared_ptr<VariableNode> z = std::make_shared<VariableNode>("z");
-		RelFactorNode one = std::make_shared<ConstantNode>("1");
+		std::shared_ptr<PredicateNode> pred = std::make_shared<PredicateNode>(yGTOne);
 
-		std::shared_ptr<RelExprNode> rel = std::make_shared<RelExprNode>(y, ComparatorOperator::GT, one);
-		std::shared_ptr<PredicateNode> pred = std::make_shared<PredicateNode>(rel);
-
-		std::vector<std::shared_ptr<StmtNode>> thenStmtList;
 		std::shared_ptr<ReadNode> readX = std::make_shared<ReadNode>(2, x);
-		thenStmtList.push_back(readX);
-
-		std::vector<std::shared_ptr<StmtNode>> elseStmtList;
+		std::vector<std::shared_ptr<StmtNode>> thenStmtList{readX};
 		std::shared_ptr<PrintNode> print = std::make_shared<PrintNode>(3, z);
-		elseStmtList.push_back(print);
+		std::vector<std::shared_ptr<StmtNode>> elseStmtList{print};
 
 		std::shared_ptr<IfNode> ifs = std::make_shared<IfNode>(1, pred, thenStmtList, elseStmtList);
+
 		std::shared_ptr<ReadNode> readY = std::make_shared<ReadNode>(4, y);
-		std::vector<std::shared_ptr<StmtNode>> stmtList{ ifs, readY };
+
+		// set up procedure
+		std::vector<std::shared_ptr<StmtNode>> stmtList{ifs, readY};
 		std::shared_ptr<ProcedureNode> proc = std::make_shared<ProcedureNode>(stmtList, "testProgram");
-		std::vector<std::shared_ptr<ProcedureNode>> procList{ proc };
+		// set up program
+		std::vector<std::shared_ptr<ProcedureNode>> procList{proc};
 
 		AST ast = std::make_shared<ProgramNode>(procList);
 
 		DesignExtractor::extractDesignElements(ast);
-		
+
 		std::vector<std::pair<int, int>> expectedFollows{
-			std::make_pair(1, 4)
+			{1, 4}
 		};
 
 		REQUIRE(EntityStager::getStagedFollows() == expectedFollows);
 
 		DesignExtractor::extractDesignElements(ast);
-
 
 		EntityStager::clear();
 	}

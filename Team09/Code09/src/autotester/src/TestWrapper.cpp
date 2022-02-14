cd8ee0775d137cdc2b071a119d762d5c306102ae
ryanlimjr <ryanlimjr@yahoo.com.sg>
2022-02-14 16:42:11 +0800

    Changed std::vector<PQLToken> to std::vector<PQLToken*>

--- Team09/Code09/src/autotester/src/TestWrapper.cpp
+++ Team09/Code09/src/autotester/src/TestWrapper.cpp
@@ -33,7 +33,7 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results) {
 
     // store the answers to the query in the results list (it is initially empty)
     // each result must be a string.
-    std::vector<PQLToken> tokens = PQL::tokenise(query);
+    std::vector<PQLToken*> tokens = PQL::tokenise(query);
     Query queryObject = PQL::parse(tokens);
     Table queryTable = QueryEvaluator::evaluate(queryObject);
     std::vector<QueryArgument> resultSynonyms = queryObject.getResultSynonyms();
--- Team09/Code09/src/spa/src/PQL/PQL.cpp
+++ Team09/Code09/src/spa/src/PQL/PQL.cpp
@@ -1,15 +1,15 @@
 #include "PQL/PQL.h"
 #include "PQL/Tokeniser.h"
-#include "PQL/Parser.h"
+#include "PQL/PQLParser.h"
 
 #include <iostream>
 
-const std::vector<PQLToken> PQL::tokenise(std::string query) {
+const std::vector<PQLToken*> PQL::tokenise(std::string query) {
 	auto tokeniser = Tokeniser(query);
 	return tokeniser.tokenise();
 }
 
-Query PQL::parse(std::vector<PQLToken> tokens) {
-	auto parser = Parser(tokens);
+Query PQL::parse(std::vector<PQLToken*> tokens) {
+	auto parser = PQLParser(tokens);
 	return parser.parse();
 }

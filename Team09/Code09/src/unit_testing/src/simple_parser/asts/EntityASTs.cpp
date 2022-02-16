12aa6936107aa251ea92445187e5275013ae1774
SoulUseless <jkyap99@gmail.com>
2022-02-16 15:27:42 +0800

    Implement boilerplate code for EntityASTs

---
+++ Team09/Code09/src/unit_testing/src/simple_parser/asts/EntityASTs.cpp
@@ -0,0 +1,129 @@
+#include "EntityASTs.h"
+
+AST EntityASTs::getAST2_1() {
+	return NonContainerStmtASTs::getAST1_1();
+}
+
+AST EntityASTs::getAST2_2() {
+	return NonContainerStmtASTs::getAST1_2();
+}
+
+AST EntityASTs::getAST2_3() {
+	return NonContainerStmtASTs::getAST1_3();
+}
+
+AST EntityASTs::getAST2_4() {
+	return NonContainerStmtASTs::getAST1_4();
+}
+
+AST EntityASTs::getAST2_5() {
+	return NonContainerStmtASTs::getAST1_5();
+}
+
+AST EntityASTs::getAST2_6() {
+	return NonContainerStmtASTs::getAST1_6();
+}
+
+AST EntityASTs::getAST2_7() {
+	return NonContainerStmtASTs::getAST1_7();
+}
+
+AST EntityASTs::getAST2_8() {
+	return NonContainerStmtASTs::getAST1_37();
+}
+
+AST EntityASTs::getAST2_9() {
+	return NonContainerStmtASTs::getAST1_38();
+}
+
+AST EntityASTs::getAST2_10() {
+	return ContainerStmtASTs::getAST1_39();
+}
+
+AST EntityASTs::getAST2_11() {
+	return ContainerStmtASTs::getAST1_40();
+}
+
+AST EntityASTs::getAST2_12() {
+	return ContainerStmtASTs::getAST1_41();
+}
+
+AST EntityASTs::getAST2_13() {
+	return ContainerStmtASTs::getAST1_52();
+}
+
+AST EntityASTs::getAST2_14() {
+	return ContainerStmtASTs::getAST1_53();
+}
+
+AST EntityASTs::getAST2_15() {
+	return ContainerStmtASTs::getAST1_54();
+}
+
+AST EntityASTs::getAST2_16() {
+	return ContainerStmtASTs::getAST1_55();
+}
+
+AST EntityASTs::getAST2_17() {
+	return ContainerStmtASTs::getAST1_56();
+}
+
+AST EntityASTs::getAST2_18() {
+	return ContainerStmtASTs::getAST1_57();
+}
+
+AST EntityASTs::getAST2_19() {
+	return ContainerStmtASTs::getAST1_58();
+}
+
+AST EntityASTs::getAST2_20() {
+	return ContainerStmtASTs::getAST1_48();
+}
+
+AST EntityASTs::getAST2_21() {
+	return ContainerStmtASTs::getAST1_49();
+}
+
+AST EntityASTs::getAST2_22() {
+	return ContainerStmtASTs::getAST1_50();
+}
+
+AST EntityASTs::getAST2_23() {
+	return ContainerStmtASTs::getAST1_64();
+}
+
+AST EntityASTs::getAST2_24() {
+	return ContainerStmtASTs::getAST1_65();
+}
+
+AST EntityASTs::getAST2_25() {
+	return ContainerStmtASTs::getAST1_66();
+}
+
+AST EntityASTs::getAST2_26() {
+	return ContainerStmtASTs::getAST1_67();
+}
+
+AST EntityASTs::getAST2_27() {
+	return ContainerStmtASTs::getAST1_68();
+}
+
+AST EntityASTs::getAST2_28() {
+	return ContainerStmtASTs::getAST1_69();
+}
+
+AST EntityASTs::getAST2_29() {
+	return ContainerStmtASTs::getAST1_70();
+}
+
+AST EntityASTs::getAST2_30() {
+	return ContainerStmtASTs::getAST1_71();
+}
+
+AST EntityASTs::getAST2_31() {
+	return ContainerStmtASTs::getAST1_72();
+}
+
+AST EntityASTs::getAST2_32() {
+	return ContainerStmtASTs::getAST1_73();
+}

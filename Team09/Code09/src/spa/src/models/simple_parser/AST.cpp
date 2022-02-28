9a736c7fed514b2f3f722056a541d04cadfdce34
SoulUseless <jkyap99@gmail.com>
2022-02-28 12:06:10 +0800

    Refactor AST to use unordered_map

--- Team09/Code09/src/spa/src/models/simple_parser/AST.cpp
+++ Team09/Code09/src/spa/src/models/simple_parser/AST.cpp
@@ -1,20 +1,33 @@
 #include "AST.h"
 
-ProgramNode::ProgramNode(std::vector<std::shared_ptr<ProcedureNode>> procList) 
+ProgramNode::ProgramNode(std::unordered_map<std::string, std::shared_ptr<ProcedureNode>> procMap)
 : Node(NodeType::PROGRAM) {
-	procedureList = procList;
+	procedureMap = procMap;
 }
 
-std::vector<std::shared_ptr<ProcedureNode>> ProgramNode::getProcedureList() const {
-	return procedureList;
+std::unordered_map<std::string, std::shared_ptr<ProcedureNode>> ProgramNode::getProcedureMap() const {
+	return procedureMap;
+}
+
+bool ProgramNode::contains(std::string procName) {
+	return procedureMap.count(procName) != 0;
+}
+
+std::shared_ptr<ProcedureNode> ProgramNode::retrieve(std::string procName) {
+	if (!contains(procName)) return nullptr;
+	return procedureMap.at(procName);
 }
 
 bool ProgramNode::operator==(const Node& other) const {
 	const ProgramNode* cast = dynamic_cast<const ProgramNode*>(&other);
 	if (cast == nullptr) return false;
-	std::vector<std::shared_ptr<ProcedureNode>> otherProcedureList = cast->getProcedureList();
 	return cast != nullptr &&
-		std::equal(begin(procedureList), end(procedureList),
-			begin(otherProcedureList), end(otherProcedureList),
+		procedureMap == cast->getProcedureMap();
+	/*
+	std::unordered_map<std::string, std::shared_ptr<ProcedureNode>> otherProcedureMap = cast->getProcedureMap();
+	return cast != nullptr &&
+		std::equal(begin(procedureMap), end(procedureMap),
+			begin(otherProcedureMap), end(otherProcedureMap),
 			[](const std::shared_ptr<ProcedureNode> lhs, const std::shared_ptr<ProcedureNode> rhs) { return *lhs == *rhs; });
+	*/
 }

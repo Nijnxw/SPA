418c4b4b76b76c9600013c1718fb4de1af25ab0b
SoulUseless <jkyap99@gmail.com>
2022-03-08 23:40:36 +0800

    Implement helper and initialisation functions

--- Team09/Code09/src/spa/src/simple_parser/CFGExtractor.cpp
+++ Team09/Code09/src/spa/src/simple_parser/CFGExtractor.cpp
@@ -2,8 +2,35 @@
 
 CFGExtractor::CFGExtractor() {}
 
-CFG CFGExtractor::extractCFG(AST ast, int totalStmts) {
-	CFG cfg(totalStmts);
+void CFGExtractor::initialiseCFG(int totalStmts) {
+	CFGExtractor::cfg.clear();
+	for (int i = 0; i < totalStmts + 1; i++) {
+		CFGExtractor::cfg.push_back({});
+	}
+}
 
-	return cfg;
+void CFGExtractor::extractCFGFromWhileNode(std::shared_ptr<WhileNode> whiles) {
+
+}
+
+void CFGExtractor::extractCFGFromIfNode(std::shared_ptr<IfNode> ifs) {
+
+}
+
+void CFGExtractor::extractCFGFromStmtLst(std::shared_ptr<StmtNode> stmt) {
+
+}
+
+void CFGExtractor::extractCFGFromProcedure(std::shared_ptr<ProcedureNode> proc) {
+
+}
+
+void CFGExtractor::extractCFGFromProcedureList(std::unordered_map<std::string, std::shared_ptr<ProcedureNode>> procMap) {
+
+}
+
+CFG CFGExtractor::extractCFG(AST ast, int totalStmts) {
+	CFGExtractor::initialiseCFG(totalStmts);
+
+	return CFGExtractor::cfg;
 }
--- Team09/Code09/src/spa/src/simple_parser/CFGExtractor.h
+++ Team09/Code09/src/spa/src/simple_parser/CFGExtractor.h
@@ -1,15 +1,26 @@
 #pragma once
 
 #include "models/simple_parser/AST.h"
+#include "models/simple_parser/IfNode.h"
+#include "models/simple_parser/WhileNode.h"
 
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
+#include <iterator>
 
 using CFG = std::vector<std::unordered_set<int>>;
 
 class CFGExtractor {
 private:
+	static inline CFG cfg;
 	CFGExtractor();
+	static void initialiseCFG(int totalStmts);
+	static void CFGExtractor::extractCFGFromWhileNode(std::shared_ptr<WhileNode> whiles);
+	static void CFGExtractor::extractCFGFromIfNode(std::shared_ptr<IfNode> ifs);
+	static void CFGExtractor::extractCFGFromStmtLst(std::shared_ptr<StmtNode> stmt);
+	static void CFGExtractor::extractCFGFromProcedure(std::shared_ptr<ProcedureNode> proc);
+	static void CFGExtractor::extractCFGFromProcedureList(std::unordered_map<std::string, std::shared_ptr<ProcedureNode>> procMap);
 
 public:
 	static CFG extractCFG(AST ast, int totalStmts);

c1cf7e594aa34aec26653bf171b7b7160741961c
KxxMxxx <lewkaiming123@gmail.com>
2022-03-11 17:30:41 +0800

    Implement boolean query handling

--- Team09/Code09/src/autotester/src/TestWrapper.cpp
+++ Team09/Code09/src/autotester/src/TestWrapper.cpp
@@ -4,44 +4,46 @@
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
+
 AbstractWrapper* WrapperFactory::createWrapper() {
-    if (wrapper == 0) wrapper = new TestWrapper;
-    return wrapper;
+	if (wrapper == 0) wrapper = new TestWrapper;
+	return wrapper;
 }
+
 // Do not modify the following line
 volatile bool AbstractWrapper::GlobalStop = false;
 AbstractWrapper::~AbstractWrapper() = default;
 
 // a default constructor
 TestWrapper::TestWrapper() {
-    // create any objects here as instance variables of this class
-    // as well as any initialization required for your spa program
+	// create any objects here as instance variables of this class
+	// as well as any initialization required for your spa program
 }
 
 TestWrapper::~TestWrapper() {
-    PKB::clearAllStores();
+	PKB::clearAllStores();
 }
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
-    // call your parser to do the parsing
-  // ...rest of your code...
-    AST ast = SPManager::parseFile(filename);
-    SPManager::extractDesign(ast);
+	// call your parser to do the parsing
+	// ...rest of your code...
+	AST ast = SPManager::parseFile(filename);
+	SPManager::extractDesign(ast);
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>&results) {
-    // call your evaluator to evaluate the query here
-      // ...code to evaluate query...
+void TestWrapper::evaluate(std::string query, std::list<std::string>& results) {
+	// call your evaluator to evaluate the query here
+	// ...code to evaluate query...
 
-      // store the answers to the query in the results list (it is initially empty)
-      // each result must be a string.
-    std::vector<PQLToken*> tokens = PQL::tokenise(query);
-    Query queryObject = PQL::parse(tokens);
-    Table queryTable = PQL::evaluate(queryObject);
-    std::vector<QueryArgument> resultSynonyms = queryObject.getResultSynonyms();
-    std::unordered_set<std::string> queryResult = PQL::getResults(queryTable, resultSynonyms);
+	// store the answers to the query in the results list (it is initially empty)
+	// each result must be a string.
+	std::vector<PQLToken*> tokens = PQL::tokenise(query);
+	Query queryObject = PQL::parse(tokens);
+	Table queryTable = PQL::evaluate(queryObject);
+	std::vector<QueryArgument> resultSynonyms = queryObject.getResultSynonyms();
+	std::unordered_set<std::string> queryResult = PQL::getResults(queryTable, queryObject);
 
-    results.insert(results.end(), queryResult.begin(), queryResult.end());
+	results.insert(results.end(), queryResult.begin(), queryResult.end());
 }
--- Team09/Code09/src/spa/src/PQL/QueryEvaluator.h
+++ Team09/Code09/src/spa/src/PQL/QueryEvaluator.h
@@ -13,11 +13,27 @@ public:
 	static Table evaluate(Query& query);
 
 private:
-	static bool evaluateBooleanClauses(std::vector<QueryClause>& clauses);
-	static std::vector<QueryClauseResult> evaluateSynWithoutClause(std::vector<QueryArgument> syns);
-	static std::vector<std::vector<QueryClauseResult>> evaluateGroupsWithoutSelect(std::vector<OptimizerGroup> groups);
-	static std::vector<std::vector<QueryClauseResult>> evaluateGroupsWithSelect(std::vector<OptimizerGroup> groups);
+	static Table evaluateBooleanQuery(const std::vector<QueryClause>& clausesWithoutSyn,
+									  const std::vector<OptimizerGroup>& groupsWithoutSelect);
+	static Table evaluateNormalQuery(const std::vector<QueryArgument>& selectSynNotInClauses,
+									 const std::vector<QueryClause>& clausesWithoutSyn,
+									 const std::vector<OptimizerGroup>& groupsWithoutSelect,
+									 const std::vector<OptimizerGroup>& groupsWithSelect);
+
+	static bool evaluateBooleanClauses(const std::vector<QueryClause>& clauses);
+	static std::vector<QueryClauseResult> evaluateSynWithoutClause(const std::vector<QueryArgument>& syns);
+	static std::vector<std::vector<QueryClauseResult>>
+	evaluateGroupsWithoutSelect(const std::vector<OptimizerGroup>& groups);
+	static std::vector<std::vector<QueryClauseResult>>
+	evaluateGroupsWithSelect(const std::vector<OptimizerGroup>& groups);
 	static Table mergeSynNotInClauseResults(const std::vector<QueryClauseResult>& synResults);
 	static bool mergeGroupWithoutSelectResults(const std::vector<std::vector<QueryClauseResult>>& groupResults);
 	static Table mergeGroupWithSelectResults(const std::vector<std::vector<QueryClauseResult>>& groupResults);
 };
+
+/*
+ * std::vector<QueryArgument> synNotInClauses;
+	std::vector<QueryClause> clausesWithoutSyn;
+	std::vector<OptimizerGroup> groupsWithSelect;
+	std::vector<OptimizerGroup> groupsWithoutSelect;
+ */

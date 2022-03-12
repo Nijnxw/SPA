13863b09f4038f2390b98471da2cc66e7dcb6d46
ryanlimjr <ryanlimjr@yahoo.com.sg>
2022-03-13 02:22:58 +0800

    add attributeRef to model query for with clause parsing and evaluation

--- Team09/Code09/src/spa/src/models/QueryArgument.cpp
+++ Team09/Code09/src/spa/src/models/QueryArgument.cpp
@@ -1,7 +1,8 @@
 #include <string>
 #include "QueryArgument.h"
 
-QueryArgument::QueryArgument(const std::string& value, EntityType type) : value(value), type(type) {}
+QueryArgument::QueryArgument(const std::string& value, EntityType type, AttributeRef attributeRef) 
+	: value(value), type(type), attributeRef(attributeRef) {}
 
 const std::string& QueryArgument::getValue() const {
     return value;
@@ -9,4 +10,8 @@ const std::string& QueryArgument::getValue() const {
 
 EntityType QueryArgument::getType() const {
 	return type;
+}
+
+AttributeRef QueryArgument::getAttrRef() const {
+	return attributeRef;
 }
--- Team09/Code09/src/spa/src/models/QueryArgument.h
+++ Team09/Code09/src/spa/src/models/QueryArgument.h
@@ -2,21 +2,23 @@
 
 #include <string>
 #include "EntityType.h"
+#include "AttributeRef.h"
 
 class QueryArgument {
 public:
-	QueryArgument(const std::string& value, EntityType type);
-
+	QueryArgument(const std::string& value, EntityType type, AttributeRef attributeRef = AttributeRef::NONE);
 	const std::string& getValue() const;
 	EntityType getType() const;
+	AttributeRef getAttrRef() const;
 
 	bool operator==(const QueryArgument& other) const {
-		return type == other.getType() && value == other.getValue();
+		return type == other.getType() && value == other.getValue() && attributeRef == other.getAttrRef() ;
 	}
 
 private:
 	std::string value;  // can either be a literal or variable name (depending on type)
 	EntityType type;
+	AttributeRef attributeRef;
 };
 
 template<>

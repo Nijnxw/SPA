92ad2d570bd9bb440a8cd056ed686b907793cf23
fabbbbbbyy <fabianchia97@gmail.com>
2022-03-13 14:27:33 +0800

    Implement NextStore

--- Team09/Code09/src/spa/src/pkb/stores/NextStore.cpp
+++ Team09/Code09/src/spa/src/pkb/stores/NextStore.cpp
@@ -1,18 +1,70 @@
 #include "NextStore.h"
 
-// ============ GETTER METHODS ==============
+/*
+Source
+1 x = x + 1;
+2 while (y = true) {
+3	y = y + 1;
+4	if (z = false) then {
+5		z = z + 1;
+	}
+	else {
+6		z = z - 1;
+	}}
+CFG, vector is 1-indexed
+1 -> {2}
+2 -> {3}
+3 -> {4}
+4 -> {5,6}
+5 -> {2}
+6 -> {2}
+*/
 
+void NextStore::clear() {
+	this->cfg.clear();
+}
 
-
-// ============ STORE METHODS ==============
-
-bool NextStore::addCFG(const std::vector<std::unordered_set<int>>* cfg) {
+bool NextStore::addCFG(const std::vector<std::unordered_set<int>>& cfg) {
 	this->cfg = cfg;
+	this->reversedCfg = reverseCFG(cfg);
+	return selfPopulateRelationships();
+}
+
+std::vector<std::unordered_set<int>> NextStore::getCFG() {
+	return cfg;
+}
+
+std::vector<std::unordered_set<int>> NextStore::getReversedCFG() {
+	return reversedCfg;
+}
+
+std::vector<std::unordered_set<int>> NextStore::reverseCFG(const std::vector<std::unordered_set<int>>& cfg) {
+	std::vector<std::unordered_set<int>> reversedCfg;
+
+	// Populating indices with empty sets
+	for (int statementNum = 0; statementNum < cfg.size(); statementNum++) {
+		std::unordered_set<int> set;
+		reversedCfg.push_back(set);
+	}
+
+	// For each next node, map next node as indice -> statementNum in the set
+	for (int statementNum = 1; statementNum < cfg.size(); statementNum++) {
+		for (const auto& nextNode : cfg[statementNum]) {
+			reversedCfg[nextNode].insert(statementNum);
+		}
+	}
+
+	return reversedCfg;
+}
+
+/* For each statement number, we add all the possible Next nodes in its unordered set. */
+bool NextStore::selfPopulateRelationships() {
+	for (int statementNum = 1; statementNum < cfg.size(); statementNum++) {
+		for (const auto& nextNode : cfg[statementNum]) {
+			StmtStmtStore::addRelationship(statementNum, nextNode);
+		}
+	}
+
 	return true;
 }
 
-// ============ OTHER METHODS ==============
-
-void NextStore::clear() {
-	this->cfg = NULL;
-}
--- Team09/Code09/src/spa/src/pkb/stores/NextStore.h
+++ Team09/Code09/src/spa/src/pkb/stores/NextStore.h
@@ -1,16 +1,22 @@
 #pragma once
 
-#include "pkb/stores/Store.h"
+#include "pkb/stores/StmtStmtStore.h"
 
-class NextStore : public Store {
+class NextStore : public StmtStmtStore {
 private:
-	const std::vector<std::unordered_set<int>>* cfg;
+	std::vector<std::unordered_set<int>> cfg;
+	std::vector<std::unordered_set<int>> reversedCfg;
 
+	std::vector<std::unordered_set<int>> reverseCFG(const std::vector<std::unordered_set<int>>& cfg);
+
+	bool selfPopulateRelationships();
 public:
-	/* Getters called by QE */
-	//std::unordered_set<std::string> getNext();
+	void clear();
 
 	/* Setters called by DE */
-	bool addCFG(const std::vector<std::unordered_set<int>>* cfg);
-	void clear();
+	bool addCFG(const std::vector<std::unordered_set<int>>& cfg);
+
+	/* Getters */
+	std::vector<std::unordered_set<int>> getCFG();
+	std::vector<std::unordered_set<int>> getReversedCFG();
 };

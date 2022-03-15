7be4902a5f30f7b09396b385ffa5deb3a31fbbd7
fabbbbbbyy <fabianchia97@gmail.com>
2022-03-15 14:04:12 +0800

    Implement NextEvaluator

--- Team09/Code09/src/spa/src/PQL/evaluators/NextEvaluator.h
+++ Team09/Code09/src/spa/src/PQL/evaluators/NextEvaluator.h
@@ -18,17 +18,22 @@ class NextEvaluator : public StmtStmtRelationshipEvaluator {
 private:
 	QueryClauseResult getRelationship(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 		bool isBooleanResult);
-	
-	QueryClauseResult getTransitiveRelationship(RelationRef relationship, const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
-		bool isBooleanResult);
-	
+
+	QueryClauseResult
+		getNextTByStatementNumber(const std::string& LHS, const std::string& RHS, EntityType RHSType);
+	QueryClauseResult
+		getNextTByStatementVariable(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType);
+	QueryClauseResult getNextTByUnderscore(const std::string& RHS, EntityType RHSType);
+
+	std::unordered_set<int> getReachableNodes(int startNode, std::vector<std::unordered_set<int>>& cfg);
+	std::unordered_map<int, std::unordered_set<int>> getAllNextTPairs();
+	void modifiedDFS(int currNode, const std::vector<std::unordered_set<int>>& reversedCfg, std::unordered_set<int>& reachableNodes, std::unordered_map<int, std::unordered_set<int>>& nextTPairs);
 public:
 	NextEvaluator();
 
 	QueryClauseResult
 	getNext(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 			  bool isBooleanResult);
-	
 	QueryClauseResult
 	getNextT(const std::string& LHS, const std::string& RHS, EntityType LHSType, EntityType RHSType,
 			bool isBooleanResult);

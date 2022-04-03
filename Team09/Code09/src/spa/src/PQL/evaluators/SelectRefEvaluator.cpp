581a718973fb5ead47ab6bc76947edb7281b0030
KxxMxxx <lewkaiming123@gmail.com>
2022-04-03 14:43:55 +0800

    Refactor SelectRefEvaluator to use reference

--- Team09/Code09/src/spa/src/PQL/evaluators/SelectRefEvaluator.cpp
+++ Team09/Code09/src/spa/src/PQL/evaluators/SelectRefEvaluator.cpp
@@ -20,25 +20,32 @@ void SelectRefEvaluator::supplementResults(Table& table, const QueryArgument& ar
 	if (ref == AttributeRef::STMT_NO || (type == EntityType::PROC && ref == AttributeRef::PROC_NAME) ||
 		(type == EntityType::CONST && ref == AttributeRef::VALUE) ||
 		(type == EntityType::VAR && ref == AttributeRef::VAR_NAME)) {
-		std::vector<std::string> stmtNum = table.at(val);
+
+		const std::vector<std::string>& stmtNum = table.at(val);
 		outputCol = {stmtNum.begin(), stmtNum.end()};
+
 	} else if (type == EntityType::CALL && ref == AttributeRef::PROC_NAME) {
-		std::vector<std::string> stmtNums = table.at(val);
-		std::unordered_map<int, std::string> stmtToProc = PKB::getCallStatementsToProcedures();
+
+		const std::vector<std::string>& stmtNums = table.at(val);
+		const std::unordered_map<int, std::string>& stmtToProc = PKB::getCallStatementsToProcedures();
 		for (const auto& num: stmtNums) {
 			outputCol.push_back(stmtToProc.at(std::stoi(num)));
 		}
+
 	} else if ((type == EntityType::READ || type == EntityType::PRINT) && ref == AttributeRef::VAR_NAME) {
+
 		std::unordered_map<int, std::string> stmtToVar;
 		if (type == EntityType::READ) {
 			stmtToVar = PKB::getReadStatementsToVariables();
 		} else {
 			stmtToVar = PKB::getPrintStatementsToVariables();
 		}
-		std::vector<std::string> stmtNums = table.at(val);
+
+		const std::vector<std::string>& stmtNums = table.at(val);
 		for (const auto& num: stmtNums) {
 			outputCol.push_back(stmtToVar.at(std::stoi(num)));
 		}
+
 	} else {
 		return;
 	}

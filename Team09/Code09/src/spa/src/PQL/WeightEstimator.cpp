65bdf15df1dccd638f3601042bb95c732aa0a655
fabbbbbbyy <fabianchia97@gmail.com>
2022-04-04 01:26:26 +0800

    Implement WeightEstimator

--- Team09/Code09/src/spa/src/PQL/WeightEstimator.cpp
+++ Team09/Code09/src/spa/src/PQL/WeightEstimator.cpp
@@ -1,5 +1,42 @@
 #include "WeightEstimator.h"
 
+const std::unordered_map<RelationRef, int> WeightEstimator::heuristicsToWeight = {
+	{RelationRef::WITH, WeightEstimator::WITH_WEIGHT},
+	{RelationRef::PATTERN_ASSIGN, WeightEstimator::PATTERN_ASSIGN_WEIGHT},
+	{RelationRef::PATTERN_IF, WeightEstimator::PATTERN_IF_WEIGHT},
+	{RelationRef::PATTERN_WHILE, WeightEstimator::PATTERN_WHILE_WEIGHT},
+	{RelationRef::CALLS, WeightEstimator::CALLS_WEIGHT},
+	{RelationRef::CALLS_T, WeightEstimator::CALLS_T_WEIGHT},
+	{RelationRef::AFFECTS, WeightEstimator::AFFECTS_WEIGHT},
+	{RelationRef::AFFECTS_T, WeightEstimator::AFFECTS_T_WEIGHT},
+	{RelationRef::FOLLOWS, WeightEstimator::FOLLOWS_WEIGHT},
+	{RelationRef::PARENT, WeightEstimator::PARENT_WEIGHT},
+	{RelationRef::NEXT, WeightEstimator::NEXT_WEIGHT},
+	{RelationRef::FOLLOWS_T, WeightEstimator::FOLLOWS_T_WEIGHT},
+	{RelationRef::PARENT_T, WeightEstimator::PARENT_T_WEIGHT},
+	{RelationRef::NEXT_T, WeightEstimator::NEXT_T_WEIGHT},
+	{RelationRef::MODIFIES, WeightEstimator::MODIFIES_WEIGHT},
+	{RelationRef::USES, WeightEstimator::USES_WEIGHT}
+};
+
 int WeightEstimator::estimateWeight(QueryClause clause) {
-	return 0;
+	int heuristicsWeight = getHeuristicsWeight(clause);
+	int statisticsWeight = getStatisticsWeight(clause);
+
+	return (heuristicsWeight * statisticsWeight) + heuristicsWeight;
+}
+
+int WeightEstimator::getHeuristicsWeight(QueryClause clause) {
+	return heuristicsToWeight.at(clause.getClauseType());
+}
+
+int WeightEstimator::getStatisticsWeight(QueryClause clause) {
+	std::vector<QueryArgument> clauseSynonyms = clause.getArguments();
+
+	int statisticsWeight = 0;
+	for (const auto& synonym : clauseSynonyms) {
+		statisticsWeight += PKB::getNumSynonymsWithType(synonym.getType());
+	}
+
+	return statisticsWeight;
 }
--- Team09/Code09/src/spa/src/PQL/WeightEstimator.h
+++ Team09/Code09/src/spa/src/PQL/WeightEstimator.h
@@ -1,10 +1,35 @@
 #pragma once
 
+#include <unordered_map>
+#include <unordered_set>
+#include "pkb/PKB.h"
 #include "models/QueryClause.h"
 #include "models/Query.h"
-#include "util/CommonUtils.cpp"
+#include "models/RelationRef.h"
 
-class WeightOptimizer {
+class WeightEstimator {
+private: 
+	static const int WITH_WEIGHT = 1;
+	static const int PATTERN_ASSIGN_WEIGHT = 2;
+	static const int PATTERN_IF_WEIGHT = 3;
+	static const int PATTERN_WHILE_WEIGHT = 3;
+	static const int CALLS_WEIGHT = 4;
+	static const int CALLS_T_WEIGHT = 5;
+	static const int AFFECTS_WEIGHT = 6;
+	static const int AFFECTS_T_WEIGHT = 7;
+	static const int FOLLOWS_WEIGHT = 8;
+	static const int PARENT_WEIGHT = 8;
+	static const int NEXT_WEIGHT = 8;
+	static const int FOLLOWS_T_WEIGHT = 9;
+	static const int PARENT_T_WEIGHT = 9;
+	static const int NEXT_T_WEIGHT = 9;
+	static const int MODIFIES_WEIGHT = 10;
+	static const int USES_WEIGHT = 11;
+
+	static const std::unordered_map<RelationRef, int> heuristicsToWeight;
+
+	static int getHeuristicsWeight(QueryClause clause);
+	static int getStatisticsWeight(QueryClause clause);
 public:
 	static int estimateWeight(QueryClause clause);
 };
